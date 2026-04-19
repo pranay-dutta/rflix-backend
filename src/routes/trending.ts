12f50cce5849a99d2283749ae74c00a80b641771
Pranay Dutta <rick15176@gmail.com>
2026-04-20 00:10:58 +0530

    refactor: add all trending items route

--- src/routes/trending.ts
+++ src/routes/trending.ts
@@ -2,22 +2,38 @@ import { Router } from "express";
 import ApiClient from "../services/api-client";
 import Movie from "@/interfaces/Movie";
 import FetchResponse from "@/interfaces/FetchResponse";
+import TvSeries from "@/interfaces/TvSeries";
 
 const router = Router();
+
+router.get("/all/:time", async (req, res) => {
+  try {
+    const { time } = req.params;
+    const apiClient = new ApiClient<FetchResponse<Movie | TvSeries>>(
+      `/trending/all/${time}`,
+    );
+
+    const response = await apiClient.get();
+    res.status(200).json(response);
+  } catch (err) {
+    console.error("Error at /trending/all", err);
+    res.status(500).json({ error: "Failed to get trending content." });
+  }
+});
 
 //Return trending movies shows [day | week]
 router.get("/movie/:time", async (req, res) => {
   try {
     const { time } = req.params;
     const apiClient = new ApiClient<FetchResponse<Movie>>(
-      `/trending/movie/${time}`
+      `/trending/movie/${time}`,
     );
 
     const response = await apiClient.get();
     res.status(200).json(response);
   } catch (err) {
     console.error("Error at /trending/movie", err);
-    res.status(500).json({ error: "Failed to trending movies." });
+    res.status(500).json({ error: "Failed to get trending movies." });
   }
 });
 
@@ -25,8 +41,8 @@ router.get("/movie/:time", async (req, res) => {
 router.get("/tv/:time", async (req, res) => {
   try {
     const { time } = req.params;
-    const apiClient = new ApiClient<FetchResponse<Movie>>(
-      `/trending/tv/${time}`
+    const apiClient = new ApiClient<FetchResponse<TvSeries>>(
+      `/trending/tv/${time}`,
     );
 
     const response = await apiClient.get();
